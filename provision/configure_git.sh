8aef4ceaea79bbf0f3198c6787f821a4d690d5d5
Wesley J. Botham <wbotham@appliedsystems.com>
2018-07-27 14:29:33 -0500

    Use vim for commit messages

--- provision/configure_git.sh
+++ provision/configure_git.sh
@@ -4,5 +4,6 @@ echo "Configuring git"
 su - vagrant -c "git config --global user.name '$1'"
 su - vagrant -c "git config --global user.email '$2'"
 su - vagrant -c "git config --global push.default current"
+su - vagrant -c "git config --global core.editor 'vim'"
 echo "*.swp" > /home/vagrant/.gitignore_global
 su - vagrant -c "git config --global core.excludesfile /home/vagrant/.gitignore_global"
