1d88ad673f12b7e179ab233802ae04a98df54cf4
Wesley J. Botham <wbotham@appliedsystems.com>
2019-06-11 10:30:41 -0500

    Fix set_up_ssh_agent.ssh by making login script path explicit

--- provision/set_up_ssh_agent.sh
+++ provision/set_up_ssh_agent.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "Modifying .bashrc to run ssh-agent on login"
-cat use_agent_on_login.txt >> /home/vagrant/.bashrc
+cat /home/vagrant/workspace/dev-environment/use_agent_on_login.txt >> /home/vagrant/.bashrc
 
 echo "echo \"If you need a key, use \\\`ssh-add ~/.ssh/KEY_NAME\\\` to give it to ssh-agent.\"" >> /home/vagrant/.bashrc
